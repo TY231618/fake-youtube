2a3a30927e83c62f277a03ca4537fdd3a442a809
Tony <anthony.c.young18@gmail.com>
2018-02-28 10:58:22 +0000

    empty files and installed youtube search

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 //we want to create a new component and this component should produce some HTML
 
 //take this generated HTML and put it in the DOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyBl22I1k7pqQBwz4ZmVABaEfpDoSBORW-Y';
+
+const App = () => {
+
+  return <div>hi</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
 
