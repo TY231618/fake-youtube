40a8fbf482c9fccd6343a6670976d705eb6e8c83
Tony <anthony.c.young18@gmail.com>
2018-02-28 11:52:48 +0000

    added state and updating it

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// class SearchBar extends React.Component {
+
+//   render() {
+//     return <input onChange={this.handleInputChange}/>
+//   }
+
+//   handleInputChange(event) {
+//     console.log(event.target.value);
+//   }
+// }
+
 class SearchBar extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = { searchTerm: '' };
+  }
+
   render() {
-    return <input />
+    return (
+      <div>
+        <input onChange={event => this.setState({ searchTerm: event.target.value })}/>
+        <br />
+        value of input: { this.state.searchTerm }
+      </div>
+    );
   }
 }
 
