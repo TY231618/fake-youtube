b5e177a213a06ef0b4233240a1b09aaf8a0eaf1a
Tony <anthony.c.young18@gmail.com>
2018-02-28 11:24:52 +0000

    refactored searchbar into class component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const SearchBar = () => {
+class SearchBar extends React.Component {
 
-  return <input />
-};
+  render() {
+    return <input />
+  }
+}
 
 export default SearchBar;
 
