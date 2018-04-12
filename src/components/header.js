160bcfc2281b37639b95c197c174136836e79081
Tony <anthony.c.young18@gmail.com>
2018-04-12 12:09:19 +0100

    changed header colour

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
 
   return (
-    <nav>
+    <nav className="red">
       <div className="nav-wrapper">
         <h2 className="center">Fake YouTube</h2>
       </div>
